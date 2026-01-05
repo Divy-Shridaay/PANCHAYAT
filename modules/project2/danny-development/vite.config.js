ad8e34421c4f9a8ad98ebb77be95b19d113f7f43
Pradeep Lodha <pradeeplodha1994@gmail.com>
2026-01-05 14:01:32 +0530

    base -p2

--- modules/project2/danny-development/vite.config.js
+++ modules/project2/danny-development/vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/p2/',
   plugins: [react()],
-  server: {
-    port: 5174,
-    host: true
-  }
 })
