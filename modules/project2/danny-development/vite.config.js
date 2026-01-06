bb56b2d5baac09d97fcebf0dbe46b4d48e4aefb2
Pradeep Lodha <pradeeplodha1994@gmail.com>
2026-01-06 16:12:18 +0530

    5174

--- modules/project2/danny-development/vite.config.js
+++ modules/project2/danny-development/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: '/magana/',
   plugins: [react()],
   server: {
     port: 5174,
-    host: true
+    host: true,
+    basename: '/p2',
   }
 })
