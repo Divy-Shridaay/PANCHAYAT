7038633c731f6f22d39706d322262271acf10edf
Pradeep Lodha <pradeeplodha1994@gmail.com>
2026-01-09 15:46:04 +0530

    app.js -5173

--- panchayat_api/src/app.js
+++ panchayat_api/src/app.js
@@ -10,10 +10,7 @@ import bankRoutes from "./routes/bank.routes.js";
 
 const app = express();
 app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://panchayat.shridaay.com"
-  ],
+  origin: ["http://localhost:5173", "http://localhost:5173"],
   credentials: true
 }));
 app.use(express.json());
