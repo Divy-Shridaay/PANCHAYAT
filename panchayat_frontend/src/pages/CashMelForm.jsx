7681665ed56f98d184cefa8c5b1a95dbba2b278c
Pradeep Lodha <pradeeplodha95@gmail.com>
2026-02-16 11:43:08 +0530

    view hidden

--- panchayat_frontend/src/pages/CashMelForm.jsx
+++ panchayat_frontend/src/pages/CashMelForm.jsx
@@ -1742,7 +1742,7 @@ const uploadExcelToServer = async () => {
                 </HStack>
               </VStack>
 
-              {form.excelData.length > 0 && (
+              {/* {form.excelData.length > 0 && (
                 <Box maxH="200px" overflowY="auto" fontSize="sm" mt={3}>
                   <table style={{ width: "100%", borderCollapse: "collapse" }}>
                     <thead>
@@ -1780,7 +1780,7 @@ const uploadExcelToServer = async () => {
                     </tbody>
                   </table>
                 </Box>
-              )}
+              )} */}
             </Box>
           </Collapse>
 
